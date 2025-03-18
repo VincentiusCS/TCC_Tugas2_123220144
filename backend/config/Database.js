b0b347a471f73ec720cad46d80fa363db3856b3d
Vincentius Sugianto <164149016+VincentiusCS@users.noreply.github.com>
2025-03-18 22:42:12 +0700

    Delete backend directory

--- backend/config/Database.js
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from "sequelize";
-
-// Nyambungin db ke BE
-const db = new Sequelize("notes", "root", "", {
-  host: "localhost",
-  dialect: "mysql",
-});
-
-export default db;
